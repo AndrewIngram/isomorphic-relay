23d9814561775a5be287987263bb4d718254ccbf
Andrew Ingram <andy@andrewingram.net>
2016-08-25 14:18:53 +0100

    Use CDN build of Relay to demo double-loading

--- examples/star-wars/webpack.config.js
+++ examples/star-wars/webpack.config.js
@@ -2,6 +2,11 @@ var path = require('path');
 
 module.exports = {
     entry: path.resolve(__dirname, 'lib', 'client.js'),
+    externals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM',      
+      'react-relay': 'Relay',
+    },    
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'lib'),
